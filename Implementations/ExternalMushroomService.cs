14f2c69207546dbef657eeac631f02dd0bc3ece0
Hallgrímur <89984159+hallgrimurt21@users.noreply.github.com>
2024-01-02 17:04:59 +0000

    Add HttpClient and implement GetMushroomByName

--- Implementations/ExternalMushroomService.cs
+++ Implementations/ExternalMushroomService.cs
@@ -1,14 +1,27 @@
+namespace ShroomCity.Services.Implementations;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 using ShroomCity.Models;
 using ShroomCity.Models.Dtos;
 using ShroomCity.Services.Interfaces;
 
-namespace ShroomCity.Services.Implementations;
-
 public class ExternalMushroomService : IExternalMushroomService
 {
-    public Task<ExternalMushroomDto?> GetMushroomByName(string name)
+    private readonly HttpClient httpClient;
+
+    public ExternalMushroomService(HttpClient httpClient) => this.httpClient = httpClient;
+    public async Task<ExternalMushroomDto?> GetMushroomByName(string name)
     {
-        throw new NotImplementedException();
+        var response = await this.httpClient.GetAsync($"https://mushrooms-api-a309dd19945c.herokuapp.com/{name}");
+
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<ExternalMushroomDto>(content);
+        }
+
+        return null;
     }
 
     public Task<Envelope<ExternalMushroomDto>?> GetMushrooms(int pageSize, int pageNumber)
