720309160504ef193318f4a1ec09ed11e3bce98d
hallgrimurt21 <hallgrimurjth@gmail.com>
2024-01-12 02:16:24 +0000

    Refactor ResearcherService class for readability

--- Implementations/ResearcherService.cs
+++ Implementations/ResearcherService.cs
@@ -8,12 +8,17 @@ public class ResearcherService : IResearcherService
 {
     private readonly IResearcherRepository researcherRepository;
 
-    public ResearcherService(IResearcherRepository researcherRepository) => this.researcherRepository = researcherRepository;
-    public Task<int?> CreateResearcher(string createdBy, ResearcherInputModel inputModel) => this.researcherRepository.CreateResearcher(createdBy, inputModel);
+    public ResearcherService(IResearcherRepository researcherRepository)
+        => this.researcherRepository = researcherRepository;
+    public Task<int?> CreateResearcher(string createdBy, ResearcherInputModel inputModel)
+        => this.researcherRepository.CreateResearcher(createdBy, inputModel);
 
-    public Task<IEnumerable<ResearcherDto>?> GetAllResearchers() => this.researcherRepository.GetAllResearchers();
+    public Task<IEnumerable<ResearcherDto>?> GetAllResearchers()
+        => this.researcherRepository.GetAllResearchers();
 
-    public Task<ResearcherDto?> GetResearcherByEmailAddress(string emailAddress) => this.researcherRepository.GetResearcherByEmailAddress(emailAddress);
+    public Task<ResearcherDto?> GetResearcherByEmailAddress(string emailAddress)
+        => this.researcherRepository.GetResearcherByEmailAddress(emailAddress);
 
-    public Task<ResearcherDto?> GetResearcherById(int id) => this.researcherRepository.GetResearcherById(id);
+    public Task<ResearcherDto?> GetResearcherById(int id)
+        => this.researcherRepository.GetResearcherById(id);
 }
